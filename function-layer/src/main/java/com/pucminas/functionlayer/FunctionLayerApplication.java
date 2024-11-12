ded3eecfdf97d75a9c8ea9e8fcb1d6d06266bff4
Jardel Nunes <jarnunesc@gmail.com>
2024-11-11 23:50:18 -0300

    commit com função AWS funcional

--- function-layer/src/main/java/com/pucminas/functionlayer/FunctionLayerApplication.java
+++ function-layer/src/main/java/com/pucminas/functionlayer/FunctionLayerApplication.java
@@ -2,7 +2,6 @@ package com.pucminas.functionlayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.pucminas"})
 public class FunctionLayerApplication {
