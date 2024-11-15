cfeda14ccf4654e2d88aeaf572bbe9d6004d926a
Jardel Nunes <jarnunesc@gmail.com>
2024-11-15 10:27:16 -0300

    adicionar endpoint para receber uma lista de pontos turisticos

--- integration/src/main/java/com/pucminas/integrations/google/vertex/VertexAIService.java
+++ integration/src/main/java/com/pucminas/integrations/google/vertex/VertexAIService.java
@@ -1,6 +1,14 @@
 package com.pucminas.integrations.google.vertex;
 
+import com.pucminas.integrations.google.vertex.dto.GeminiResponse;
+
+import java.util.List;
+
 public interface VertexAIService {
+
+    GeminiResponse processPrompt(String prompt);
+
+    String generateLocationDescription(List<String> locationsName);
 
     String generateLocationDescription(String locationName);
 }
--- integration/src/main/java/com/pucminas/integrations/google/vertex/VertexAIServiceImpl.java
+++ integration/src/main/java/com/pucminas/integrations/google/vertex/VertexAIServiceImpl.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
+
 @Service
 @CommonsLog
 public class VertexAIServiceImpl implements VertexAIService {
@@ -30,20 +32,7 @@ public class VertexAIServiceImpl implements VertexAIService {
     }
 
     @Override
-    public String generateLocationDescription(String locationName) {
-        final String prompt = message.get("vertex-ai.gemini.location.description.prompt", locationName);
-        final GeminiResponse response = processPrompt(prompt);
-        try {
-            return response.getCandidates().stream().findFirst().map(GeminiCandidate::getContent)
-                    .map(it -> it.getParts().stream().iterator().next()).map(GeminiPart::getText)
-                    .orElse(null);
-        } catch (Exception e) {
-            log.error(e);
-            return null;
-        }
-    }
-
-    private GeminiResponse processPrompt(final String prompt) {
+    public GeminiResponse processPrompt(final String prompt) {
         final GeminiRequest geminiRequest = GeminiRequest.GeminiRequestBuilder.prompt(prompt).build();
         return WebClient.builder().baseUrl(vertexProperties.getGeminiUrl()).build()
                 .post()
@@ -52,6 +41,26 @@ public class VertexAIServiceImpl implements VertexAIService {
                 .retrieve()
                 .bodyToMono(GeminiResponse.class)
                 .block();
+    }
+
+    @Override
+    public String generateLocationDescription(String locationName) {
+        final String prompt = message.get("vertex-ai.gemini.location.description.prompt", locationName);
+        final GeminiResponse response = processPrompt(prompt);
+        return processGeminiResponse(response);
+    }
+
+    @Override
+    public String generateLocationDescription(List<String> locationsName) {
+        final String prompt = message.get("vertex-ai.gemini.location.list.description.prompt", String.join(", ", locationsName));
+        final GeminiResponse response = processPrompt(prompt);
+        return processGeminiResponse(response);
+    }
+
+    private String processGeminiResponse(final GeminiResponse response) {
+        return response.getCandidates().stream().findFirst().map(GeminiCandidate::getContent)
+            .map(it -> it.getParts().stream().iterator().next()).map(GeminiPart::getText)
+            .orElse(null);
     }
 
 }
