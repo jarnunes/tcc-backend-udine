723859153863bdb2b060e7c08e7430cf3efdfc6f
Jardel Nunes <jarnunesc@gmail.com>
2024-11-15 11:24:20 -0300

    ajustar api do google places para retornar pontos turísticos

--- integration/src/main/java/com/pucminas/integrations/google/places/PlacesServiceImpl.java
+++ integration/src/main/java/com/pucminas/integrations/google/places/PlacesServiceImpl.java
@@ -1,10 +1,8 @@
 package com.pucminas.integrations.google.places;
 
-import com.pucminas.integrations.google.places.dto.Place;
 import com.pucminas.integrations.google.places.dto.PlacesRequest;
 import com.pucminas.integrations.google.places.dto.PlacesResponse;
 import lombok.extern.apachecommons.CommonsLog;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,7 +20,6 @@ public class PlacesServiceImpl implements PlacesService {
     }
 
     public PlacesResponse getNearbyPlaces(PlacesRequest request) {
-
         final String url = UriComponentsBuilder.fromHttpUrl(properties.getGooglePlacesURL())
                 .queryParam("location", request.getLatitude() + "," + request.getLongitude())
                 .queryParam("radius", request.getRadius())
